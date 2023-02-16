a8f4d3a5aa774921ba1abaf21d49eabe78e7bbbb
Corches Ciprian <cipriancorches@gmail.com>
2023-02-16 19:34:32 +0200

    problema 1 si 2 (2 = ?)

--- tema-14/functii.h
+++ tema-14/functii.h
@@ -22,3 +22,34 @@ void afisare(int v[], int dim) {
 	cout << endl;
 
 }
+int nrMinimSecventeStrictCrescatoare(int v[], int dim) {
+	int ctr = 0;
+	for (int i = 0; i < dim; i++) {
+		if (v[i] < v[i + 1]) {
+			ctr++;
+		}
+	}
+	return ctr;
+}
+void secventeEgaleCuSumaAfisare(int v[], int dim,int s) {
+	int stg = 0;
+	int dr = 0;
+	for (int i = 0; i < dim; i++) {
+		for (int j = i; j < dim; j++) {
+			int sum = 0;
+			for (int z = i; z < j; z++) {
+				sum += v[z];
+			}
+			if (sum == s) {
+				stg = i; dr = j;
+			}
+		}
+		for (int f = 0; f < dim; f++) {
+			if (f >= stg && f < dr) {
+				cout << v[f] << " ";
+			}
+		}
+		cout << endl;
+	}
+	
+}
--- tema-14/solutii.h
+++ tema-14/solutii.h
@@ -1,7 +1,20 @@
-#pragma once
+﻿#pragma once
 #include "functii.h"
 using namespace std;
 
 void sol1() {
-	//
+	//(38) Se consideră un tablou unidimensional cu n(<100) elemente naturale. Să se determine numărul minim de subşiruri strict crescătoare de valori consecutive în care poate fi partiționat vectorul.Prin subşir se întelege o secvență de elemente din vectorul inițial ce nu se află neapărat pe poziții consecutive.
+	int v[100], d = 0;
+	citire(v, d);
+	int ctr = nrMinimSecventeStrictCrescatoare(v, d);
+	cout << "Numarul minim de secvente crescatoare este: " << ctr << endl;
+}
+void sol2() {
+	//(39) Se consideră un tablou unidimensional cu n(<100) elemente întregi. Să se determine toate secvențele de elemente de pe poziții consecutive, care au suma egală cu S.Fiecare secvență de elemente va fi afişată pe câte o linie pe ieşirea standard.
+	int v[100], d = 0;
+	citire(v, d);
+	cout << "Introduecti suma: " << endl;
+	int s;
+	cin >> s;
+	secventeEgaleCuSumaAfisare(v, d,s); ////?
 }
